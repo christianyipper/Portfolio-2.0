73614002973e69aa2082bbf24a9bba8324f9be36
Christian Yip <christianyippers@gmail.com>
2026-03-24 03:17:09 -0700

    Updated Hero

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -2,19 +2,22 @@
 
 export default function Home() {
     return (
-        <section className="relative flex justify-center items-center h-[calc(100vh-64px)]">
+        <section className="relaitve grid grid-rows-[1fr_auto] justify-center items-center h-[calc(100vh-32px)] rounded-4xl">
             <video
-                src="/img/phone-bg.mp4"
+                src="/img/yipper-logo-bg.mp4"
                 autoPlay
                 loop
                 muted
                 playsInline
-                className="absolute h-full w-auto"
+                className="h-full min-h-0 object-cover mx-auto"
             />
-            <div className="absolute z-10 w-full p-8 bg-amber-500/0">
-                <h2 className="font-zuume font-bold text-[64px] pb-4">Home</h2>
-            </div>
-            <div className="absolute bottom-0 w-full h-12 flex flex-row items-center justify-center">
+            <div className="relative flex flex-col items-center justify-center p-8">
+                <div className="flex flex-col w-full justify-center items-center">
+                    <span className="font-zuume font-bold text-[48px]">Stop settling for static.</span>
+                    <a href="mailto:christian@yipper.ca" className="group relative h-14 -mt-4 flex items-center justify-center rounded-2xl text-[#00BBFF] hover:text-white cursor-pointer hover:bg-[#00BBFF] transition duration-200 ease ">
+                        <span className="font-zuume font-bold text-[48px] inline-block transition-transform duration-200 ease group-hover:scale-90">Start your project.</span>
+                    </a>
+                </div>
                 <img
                     src="/svg/down-arrow.svg"
                     alt="scroll down"
